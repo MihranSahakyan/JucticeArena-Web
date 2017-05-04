0ce1679c9971cdcbb401d195fa89860c4b943f16
Mihran1999 <mihransahakyan9999@gmail.com>
2017-05-05 00:20:29 +0400

    added rotating animation to left part

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	//Rotater
+    $(".layer").click(function() {
+    	$(".heroes").toggleClass("heroes_display_none");
+    	$(this).toggleClass("prop");
+    });
+
+    // Slider
+    var names = ["aaa", "asdsa", "jkklk"];
+    
+    for (var i = 0; i < names.length; ++i) {
+    	names[i]
+    }
+});
